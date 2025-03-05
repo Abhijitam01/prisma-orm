459dbd1a53db9f73725d0d43e93bc9010f93bb7d
unknown <abhijitamdubey31@gmail.com>
2025-03-06 03:35:12 +0530

    expressionify

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,46 @@
+import express from 'express';
 import { PrismaClient } from "@prisma/client";
+import { parse } from 'path';
 
+const app = express();
 const client = new PrismaClient();
 
-async function createUser(){
-       const user =  await   client.user.findFirst({
+app.get("/users" , async(req , res) => {
+    const users =  await   client.user.findFirst() as unknown as string;
+        res.json({
+            users
+        })
+
+})
+
+app.get("/todos/:id" ,async (req , res) => {
+    const id = req.params.id;
+    const user = await client.user.findFirst({
+        where : {
+            id : parseInt(id)
+        },
+        select:{
+            todos:true
+        }
+    })
+    res.json({
+        user
+    })
+})
+
+app.listen(3000)
+
+async function getUser(){
+    const user = await client.user.findFirst({
         where:{
             id :1
+        },
+        include:{
+            todos:true
         }
     })
 
     console.log(user)
-} 
+}
 
-createUser();
+getUser();
