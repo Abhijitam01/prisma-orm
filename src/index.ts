707f9600e6e3d31d1d7bf8200a848b51b7fac7e3
unknown <abhijitamdubey31@gmail.com>
2025-03-06 03:09:28 +0530

    understanding the relation

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-console.log("Hello World");
+import { PrismaClient } from "@prisma/client";
+
+const client = new PrismaClient();
+
+async function createUser(){
+       const user =  await   client.user.findFirst({
+        where:{
+            id :1
+        }
+    })
+
+    console.log(user)
+} 
+
+createUser();
